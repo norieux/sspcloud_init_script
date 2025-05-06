75d1fb69862fc171b5fe180d1cc3d6c1f24f4498
Nicolas ORIEUX <nicolas.orieux@neuf.fr>
2025-05-06 16:52:50 +0200

    Update vscode_init.sh

--- vscode_init.sh
+++ vscode_init.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
+
+# This init script install various useful VScode extensions
+# NB : only extensions from the Open VSX Registry (https://open-vsx.org/) can be installed on code-server
+# Expected parameters : None
+
+# CONFORT EXTENSIONS -----------------
+
+code-server --install-extension ms-toolsai.jupyter
+code-server --install-extension ms-python.python
+code-server --install-extension charliermarsh.ruff
+code-server --install-extension aaron-bond.better-comments
+code-server --install-extension ms-toolsai.datawrangler
+code-server --install-extension GitHub.copilot
+code-server --install-extension GitHub.copilot-chat
+
 
